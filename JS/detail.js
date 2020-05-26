e5cdd35e337aaf73645a397b78d9679ff87892e9
kduckk00 <kduckk00@gmail.com>
2020-05-26 18:53:34 +0900

    20200526_detail2

--- JS/detail.js
+++ JS/detail.js
@@ -1,60 +1,33 @@
 $(function() {
     $("#Period_Direct").hide();
 
+
+    var repeat;
     $("#period").change(function(){
         console.log("change");
         if($("#period").val() == "direct") {
             console.log("show");
-            $(this).hide();
-            // $(this).attr('class','hide');
-            $("#Period_Direct").show();
             
+            $("#Period_Direct").show();
         } else {
             console.log("hide");
-            $("Period_Direct").hide();
+            $("#Period_Direct").hide();
         }
     })
 });
 
+$(function () {
+    $('#period').change(function() {
+        console.log(this.value);
+    });
 
-// $(function(){
-//     //직접입력 인풋박스 기존에는 숨어있다가
-
-// $("#Period_Direct").hide();
-
-
-
-// $("#selbox").change(function() {
-
-      
-
-//               //직접입력을 누를 때 나타남
-
-//       if($("#selbox").val() == "direct") {
-
-//           $("#Period_Direct").show();
-
-//       }  else {
-
-//           $("#Period_Direct").hide();
-
-//       }
-
-      
-
-//   }) 
-
-  
-
-// });
-
-
-
-// $(document).ready(function() {
-//     $('#period').change(function() {
-//         var period_Direct = $('#period').val();
-//         if(period_Direct == 'direct') {
-//             $("#period").replaceWith("<input type='text' id='direct' placeholder='직접입력'>");
-//         }
-//     })
-// })
+    $('#Button_NewList').click(function() {
+        console.log($('#period').val());
+        if ($('#period').val() == "") {
+            alert("입력하세요");
+            return;
+        } else {
+            location.href = "Congratulations.html";
+        }
+    });
+});
