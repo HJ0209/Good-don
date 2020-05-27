e082c1f0f7369211c6c35732cee1198de36c576d
StolenMoments <unknown9732@naver.com>
2020-05-27 14:19:44 +0900

    메뉴토글

--- JS/limit.js
+++ JS/limit.js
@@ -1,7 +1,25 @@
-$("#limit").click(function(){
-    swal({
-        title: "리미트소비",
-        text: "빠른 시일 내에 서비스를 제공하겠습니다.",
-        button: "확인",
-    });
-    });
+        $("#limit").click(function(){
+            swal({
+                title: "리미트소비",
+                text: "곧 제공될 서비스입니다.",
+                button: "확인",
+            });
+            });
+
+        $("button").click(function(){
+            swal({
+                title: "로그아웃",
+                text: "로그인 페이지로 이동합니다",
+                buttons: true,
+                dangerMode:true,
+            })
+            .then((Logout)=> {
+                if(Logout) {
+                    swal("로그아웃합니다");
+                    document.location.href="loginpage.html"
+                } else {
+                    swal("로그인을 유지합니다");
+                }
+            })
+        });
+            
